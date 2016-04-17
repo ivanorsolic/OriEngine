ad1cc7c1ba5a365191c01f711269f82599e7d5ec
originx <marioorsolic@gmail.com>
2016-04-17 17:52:58 +0200

    migrate to gradle

---
+++ src/main/java/com/oriengine/renderEngine/RawModel.java
@@ -0,0 +1,22 @@
+package com.oriengine.renderEngine;
+
+public class RawModel {
+
+		private int vaoID;
+		private int vertexCount;
+		
+		public RawModel(int vaoID, int vertexCount){
+			this.vaoID = vaoID;
+			this.vertexCount = vertexCount;
+		}
+
+		public int getVaoID() {
+			return vaoID;
+		}
+
+		public int getVertexCount() {
+			return vertexCount;
+		}
+		
+}
+
